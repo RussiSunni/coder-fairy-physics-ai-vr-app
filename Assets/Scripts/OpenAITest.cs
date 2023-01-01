93cd661841f7ba24c2a40a881a5bbfed2b842676
RussiSunni <jonathandyason@hotmail.com>
2023-01-01 15:22:03 +0200

    Switching to VR app

--- Assets/Scripts/OpenAITest.cs
+++ Assets/Scripts/OpenAITest.cs
@@ -14,8 +14,8 @@ public class OpenAITest : MonoBehaviour
     public Text questionText;
     public Text answerText;
     private void Start()
-    {        
-    }    
+    {
+    }
 
     private static string callOpenAI(int tokens, string input, string engine, double temperature, int topP, int frequencyPenalty, int presencePenalty)
     {
@@ -67,6 +67,6 @@ public class OpenAITest : MonoBehaviour
         // Receive question.
         var answer = callOpenAI(250, questionText.text, "text-davinci-002", 0.7, 1, 0, 0);
         // Print answer to screen.
-        answerText.text = answer;        
+        answerText.text = answer;
     }
 }
