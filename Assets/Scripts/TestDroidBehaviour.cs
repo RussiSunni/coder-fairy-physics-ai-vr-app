cbb3e5945e4629a57524ef5c1cb9e881551c7547
RussiSunni <jonathandyason@hotmail.com>
2023-04-14 17:12:45 +0200

    Added test droid for physics exercise prototype.

---
+++ Assets/Scripts/TestDroidBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class TestDroidBehaviour : MonoBehaviour
+{
+    public GameObject goal;
+    Vector3 direction;
+    public TMP_Text speedText;
+    public int speed = 0;
+
+    void Start()
+    {
+        speedText.text = speed.ToString();
+    }
+
+    public void IncreaseSpeed()
+    {
+        speed++;
+        speedText.text = speed.ToString();
+    }
+
+    public void DecreaseSpeed()
+    {
+        if (speed > 0)
+        {
+            speed--;
+            speedText.text = speed.ToString();
+        }
+    }
+
+    void LateUpdate()
+    {
+        // Work out direction vector.
+        direction = goal.transform.position - transform.position;
+
+        // Face goal.
+        transform.LookAt(goal.transform.position);
+
+
+        if (direction.magnitude > 2)
+        {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            transform.position = transform.position + velocity;
+        }
+    }
+}
