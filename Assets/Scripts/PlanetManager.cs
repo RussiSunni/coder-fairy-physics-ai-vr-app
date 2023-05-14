6be9100c76686d9a7f93627fad075672980ee2be
RussiSunni <jonathandyason@hotmail.com>
2023-05-14 10:54:14 +0200

    Start menu

--- Assets/Scripts/PlanetManager.cs
+++ Assets/Scripts/PlanetManager.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlanetManager : MonoBehaviour
 {    
     void Start()
     {
-        // Not sure if Moon gravity.
-        Physics.gravity = new Vector3(0, -1.0F, 0);
+        // Create a temporary reference to the current scene.
+        Scene currentScene = SceneManager.GetActiveScene();
+        // Retrieve the name of this scene.
+        string sceneName = currentScene.name;
+
+        if (sceneName == "The Earth")
+        {
+            // Do something...
+        }
+        else if (sceneName == "The Moon")
+        {
+            Physics.gravity = new Vector3(0, -1.0F, 0);
+        }
     }   
 }
